2324b88d2860b97e0fbb8641590de8798e544e97
ontheway12138 <1358227862@qq.com>
2019-08-16 10:11:44 +0800

    update

--- assets/Script/Board.ts
+++ assets/Script/Board.ts
@@ -3,7 +3,7 @@ import MapItem from "./MapItem";
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class GameBoard extends cc.Component {
+export default class Board extends cc.Component {
 
     @property(cc.Label)
     private scoreLabel: cc.Label = null;
@@ -14,23 +14,24 @@ export default class GameBoard extends cc.Component {
     @property(cc.Integer)
     private tileHight: number = 120;
 
+    // 整体向上偏移 250
+    @property(cc.Integer)
+    private pos_y: number = 250;
+
     @property(cc.Prefab)
     private mapItem: cc.Prefab = null;
 
-    private hexes: [cc.Vec2[]] = [[]];
+    // 存放每个节点
+    public boardFrameList: Array<MapItem> = new Array<MapItem>();
 
     protected onLoad() {
         this.setUpScore();
         this.setHexagonMap();
     }
 
-    protected start() {
-        //
-    }
-
     private setUpScore() {
-        const oldScore = cc.sys.localStorage.getItem("score") as string || "0";
-        this.scoreLabel.string = oldScore;
+        const curScore = cc.sys.localStorage.getItem("score") as string || "0";
+        this.scoreLabel.string = curScore;
     }
 
     private setHexagonMap() {
@@ -39,16 +40,10 @@ export default class GameBoard extends cc.Component {
             const r1 = Math.max(-this.hexSide, -q - this.hexSide);
             const r2 = Math.min(this.hexSide, -q + this.hexSide);
             for (let r = r1; r <= r2; r++) {
-                const col = q + this.hexSide;
-                const row = r - r1;
-                if (!this.hexes[col]) {
-                    this.hexes[col] = [];
-                }
-                this.hexes[col][row] = this.hex2pixel({ q, r }, this.tileHight);
+                const pos = this.hex2pixel({ q, r }, this.tileHight);
+                const hexPos = cc.v2(q, r);
+                this.setUpMapItem(pos, hexPos);
             }
-        }
-        for (const hexs of this.hexes) {
-            this.setUpMapItem(hexs);
         }
     }
 
@@ -56,17 +51,20 @@ export default class GameBoard extends cc.Component {
         // 棋盘六角网格，坐标系转换像素方法
         const size = h / 2;
         const x = size * Math.sqrt(3) * (hex.q + hex.r / 2);
-        const y = ((size * 3) / 2) * hex.r;
+        const y = ((size * 3) / 2) * hex.r + this.pos_y;
         return cc.v2(x, y);
     }
 
-    private setUpMapItem(hexs: cc.Vec2[]) {
-        for (const hex of hexs) {
-            const node = cc.instantiate(this.mapItem);
-            const mapItem = node.getComponent(MapItem);
-            mapItem.color = COLOR.LIGHTGRAY;
-            node.position = hex;
-            node.parent = this.node;
-        }
+    private setUpMapItem(pos: cc.Vec2, hexPos: cc.Vec2) {
+        const node = cc.instantiate(this.mapItem);
+        const mapItem = node.getComponent(MapItem);
+        mapItem.color = COLOR.LIGHTGRAY;
+        node.position = pos;
+        node.parent = this.node;
+        mapItem.isFill = false;
+        mapItem.pos = pos;
+        mapItem.hexPos = hexPos;
+        this.boardFrameList.push(mapItem);
     }
+
 }
--- assets/Script/TileItem.ts
+++ assets/Script/TileItem.ts
@@ -1,5 +1,6 @@
-import { TITLES } from "./Config";
+import { TITLES, COLOR } from "./Config";
 import MapItem from "./MapItem";
+import Board from "./Board"
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -7,23 +8,38 @@ export default class TileItem extends cc.Component {
     @property(cc.Integer)
     private tileHight: number = 122;
 
+    @property(cc.Float)
+    private tileScale: number = 0.7;
+
     @property(cc.Prefab)
     private blockItem: cc.Prefab = null;
+ 
+    private prePos: cc.Vec2 = null;
 
-    private _drapNode: cc.Node = null;
+    private theColor: cc.Color = null;
+
+    @property(Board)
+    private board: Board = null;
+
+    // 棋盘与方块重合部分
+    private fillTiles: Array<MapItem> = new Array<MapItem>();
+
 
     protected onLoad() {
+        this.prePos = this.node.getPosition();
         this.setUpTile();
         this.addTouchEvent();
     }
 
     private setUpTile() {
+        this.node.setPosition(this.prePos);
         const hexData = this.getRandomHex();
         const hexPx = hexData.list.map((hexAtrr) => {
             return this.hex2pixel(hexAtrr, this.tileHight);
         });
         this.setUpTileItem(hexPx, hexData.color);
-        this.node.scale = 0.7;
+        this.node.scale = this.tileScale;
+        this.theColor = hexData.color;
     }
 
     private hex2pixel(hexArr: number[], h: number) {
@@ -58,29 +74,66 @@ export default class TileItem extends cc.Component {
     }
 
     private addTouchEvent() {
-        this.node.on(cc.Node.EventType.TOUCH_START, (event: cc.Event.EventTouch) => {
-            this._drapNode = cc.instantiate(this.node);
-            this._drapNode.scale = 1;
-            this._drapNode.parent = this.node.parent.parent.getChildByName("gameBoard");
-            for (const child of this._drapNode.children) {
-                child.scale = 0.8;
-            }
-        });
+        this.node.on(cc.Node.EventType.TOUCH_START, (event: cc.Event.EventTouch) => {});
 
         this.node.on(cc.Node.EventType.TOUCH_MOVE, (event: cc.Event.EventTouch) => {
+            
             const delta = event.touch.getDelta();
-            if (!this._drapNode) {
-                return;
-            }
-            this._drapNode.position = this._drapNode.position.add(delta);
+            this.node.position = this.node.position.add(delta);
+            this.checkCollision(event);
+
         });
 
         this.node.on(cc.Node.EventType.TOUCH_CANCEL, (event: cc.Event.EventTouch) => {
-            //
+            this.tileDrop();
         });
 
         this.node.on(cc.Node.EventType.TOUCH_END, (event: cc.Event.EventTouch) => {
-            //
+            this.tileDrop();
         });
     }
+
+    private checkCollision(event) {
+        const tiles = this.node.children;
+        this.fillTiles = []; // 保存棋盘与方块重合部分。
+
+        const distance = 50;
+        const boardFrameList = this.board.boardFrameList;
+        for(const it of boardFrameList) {
+            // 当前节点被填满直接退出
+            if(it.isFill) continue;
+            var flag = true;
+
+            for(const child of this.node.children) {
+                const pos = this.node.position.add(child.position);
+
+                if(!it.isFill && it.pos.sub(pos).mag() < distance)
+                    flag = false, this.fillTiles.push(it);
+            }
+            // 没有和当前移动元素相交
+            if(flag) it.color = COLOR.LIGHTGRAY, it.opacity = 255;
+        }
+
+        // 可以放的位置和当前位置相等
+        if(this.fillTiles.length == this.node.children.length){
+            for(const it of this.fillTiles)
+                it.color = this.theColor, it.opacity = 150;
+        }
+    }
+
+    private tileDrop(){
+        // 可以放到当前网格中
+        if(this.fillTiles.length == this.node.children.length){
+            for(const it of this.fillTiles) 
+                it.color = this.theColor, it.opacity = 255, it.isFill = true;
+
+            // 删除当前节点， 重新添加新的节点
+            this.node.removeAllChildren();
+            this.setUpTile();
+        } else {
+            // 回到之前的位置
+            this.node.setPosition(this.prePos);
+        }
+    }
+
 }
